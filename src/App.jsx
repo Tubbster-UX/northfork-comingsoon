7874346c71894daa119671df3257b322fe0f5aac
Tubbster-UX <Alexavierbrink@gmail.com>
2024-05-27 20:08:04 -0600

    chore: Update background image URL in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="relative h-screen">
       <div
-        className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1597691306883-ac4e882e7cc4?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-no-repeat bg-center bg-cover z-10"
+        className="absolute inset-0 grayscale-[40%] bg-[url('/dirt-min.jpg')] bg-no-repeat bg-center bg-cover z-10"
       ></div>
       <div className="absolute inset-0 bg-black opacity-50 z-20"></div>
       <div className="absolute z-40 flex-col flex items-center justify-center inset-0">
